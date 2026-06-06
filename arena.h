71bdd16186a11044718e4a784b1dd26ebdaeb13d
someguy0555 <gjonas2005@gmail.com>
2026-06-06 20:55:47 +0300

    Init commit

---
+++ arena.h
@@ -0,0 +1,26 @@
+#ifndef ARENA_H
+#define ARENA_H
+
+#define ARENA_INITIAL_CAPACITY 256
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+typedef struct
+{
+    void*  body;
+    size_t capacity;
+    size_t size;
+}
+Arena;
+
+Arena  init_arena              ();
+void   free_arena              (Arena* arena);
+
+void   arena_alloc_and_set_size(Arena* arena, size_t new_size);
+size_t push_arena              (Arena* arena, size_t obj_size, void* obj);
+size_t pop_arena               (Arena* arena, size_t obj_size);
+
+#endif
---
+++ main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "arena.h"
+
+typedef struct
+{
+    char body[1024];
+}
+SerialObject;
+
+typedef struct
+{
+    int32_t x;
+    int32_t y;
+}
+Vec2D;
+
+int main()
+{
+    SerialObject objs[1024];
+    Vec2D vec;
+    Arena arena = init_arena();
+
+    size_t serial_off = push_arena(&arena, sizeof(SerialObject)* 1024, &objs);
+    size_t vec_off    = push_arena(&arena, sizeof(Vec2D), &vec);
+
+    printf("%ld:%ld:%ld\n", serial_off, vec_off, arena.capacity);
+    return 0;
+}
